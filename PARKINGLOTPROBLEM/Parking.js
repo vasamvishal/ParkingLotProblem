0f4fa15de83d00a7f18f5ed82e9b2ff8d59f3d73
vishalvasam <VishalVasam12>
2020-01-02 11:20:58 +0530

    ADD:Test case and  code for driver is able to unpark their car

--- PARKINGLOTPROBLEM/Parking.js
+++ PARKINGLOTPROBLEM/Parking.js
@@ -1,6 +1,14 @@
 //Welcome to parking lot problem//
 class Parking{
+    car;
     park(car){
+        this.car=car;
+        return true;
+    }
+    unPark()
+    {
+        console.log(this.car);
+        this.car=null;
         return true;
     }
 }
--- test/ParkingTest.js
+++ test/ParkingTest.js
@@ -1,9 +1,18 @@
-var assert=require('assert');
-var Parking=require("../PARKINGLOTPROBLEM/Parking");
+var assert = require('assert');
+var Parking = require("../PARKINGLOTPROBLEM/Parking");
+let car = new Object();
 describe("parking lot problem for usercase 1", () => {
+
     it("Driver should be able to park is Car", () => {
-        let VehicleObject=new Parking();
-        let result=VehicleObject.park(new Object());
-        assert.equal(true,result);
+        let VehicleObject = new Parking();
+        let result = VehicleObject.park(car);
+        assert.equal(true, result);
+    });
+
+    it("Driver should be able to unpark is Car", () => {
+        let VehicleObject = new Parking();
+        VehicleObject.park(car);
+        let result = VehicleObject.unPark();
+        assert.equal(true, result);
     });
 });
